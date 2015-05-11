10e66245878c1842699cf1757ee8753ed997888d
root <cm288@njit.edu>
2015-05-11 03:26:19 -0400

    most recent commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 echo '<h4> Top 10 Colleges For Each Question Below:</h4>';
 
-//Query Links 1-6
+//Query Links #'s 1-6
 echo '<a href="index.php?query=q1">Colleges with the highest percentage of women students</a><br>';
 echo '<a href="index.php?query=q2">Colleges with the highest percentage of male students</a><br>';
 echo '<a href="index.php?query=q3">Colleges with the largest endowment overall</a><br>';
@@ -10,24 +10,18 @@ echo '<a href="index.php?query=q4">Colleges with the largest enrollment of fresh
 echo '<a href="index.php?query=q5">Colleges with the highest revenue from tuition</a><br>';
 echo '<a href="index.php?query=q6">Colleges with the lowest non-zero tuition revenue</a><br>';
 
-
+//Query Links #'s 7-16
 echo '<h4>Top 10 Colleges by Region</h4>';
-echo '<a href="index.php?query=">US Service Schools</a><br>';
-echo '<a href="index.php?query=">New England</a><br>';
-echo '<a href="index.php?query=">Mid East</a><br>';
-echo '<a href="index.php?query=">Great Lakes</a><br>';
-echo '<a href="index.php?query=">Plains</a><br>';
-echo '<a href="index.php?query=">Southeast</a><br>';
-echo '<a href="index.php?query=">Southwest</a><br>';
-echo '<a href="index.php?query=">Rocky Mountains</a><br>';
-echo '<a href="index.php?query=">Far West</a><br>';
-echo '<a href="index.php?query=">Outlying Areas</a><br>';
-
-
-
-
-
-
+echo '<a href="index.php?query=q7">US Service Schools</a><br>';
+echo '<a href="index.php?query=q8">New England</a><br>';
+echo '<a href="index.php?query=q9">Mid East</a><br>';
+echo '<a href="index.php?query=q10">Great Lakes</a><br>';
+echo '<a href="index.php?query=q11">Plains</a><br>';
+echo '<a href="index.php?query=q12">Southeast</a><br>';
+echo '<a href="index.php?query=q13">Southwest</a><br>';
+echo '<a href="index.php?query=q14">Rocky Mountains</a><br>';
+echo '<a href="index.php?query=q15">Far West</a><br>';
+echo '<a href="index.php?query=q16">Outlying Areas</a><br>';
 
 //DB Connection
 error_reporting(-1);
@@ -180,8 +174,90 @@ if($_GET['query']=="q6")
 }
 
 
+//Query 7 print (US Service  Schoools)
+if($_GET['query']=="q7")
+{
+	$sql = 'select instnm, F1H02 from (select UNITID, instnm from hd2013 where OBEREG = "0") as test1 left join (select UNITID, F1H02 from f1213_f1a) as test2 on test1.UNITID = test2.UNITID WHERE NOT F1H02 IS NULL and not F1H02 = 0 group by test1.instnm order by F1H02 desc limit 10';
+	
+	if(!$result = $db->query($sql))
+                die('error'.$db->error);
+
+	 	echo '<table border="1" style="width:100%">';
+                echo "<tr>";
+                echo "<th>School</th>";
+                echo "<th>Endowment</th>";
+                echo "</tr>";
+
+
+		while($row = $result->fetch())
+                {
+                        echo "<tr>";
+                        echo "<td>".$row['instnm']."</td>";
+                        echo "<td>".$row['F1H02']."</td>";
+                        echo "</tr>";
+                }
+}
 
 
 
+	$a2 = 'select instnm, F1A01 from (select UNITID, instnm from hd2013 where OBEREG = "0") as test1 left join (select UNITID, F1A01 from f1213_f1a) as test2 on test1.UNITID = test2.UNITID WHERE NOT F1A01 IS NULL and not F1A01 = 0 group by test1.instnm order by F1A01 desc limit 10';
+	$a3 = 'select instnm, F1A09 from (select UNITID, instnm from hd2013 where OBEREG = "0") as test1 left join (select UNITID, F1A09 from f1213_f1a) as test2 on test1.UNITID = test2.UNITID WHERE NOT F1A09 IS NULL and not F1A09 = 0 group by test1.instnm order by F1A09 desc limit 10';
+	$a4 = 'select instnm, F1B01 from (select UNITID, instnm from hd2013 where OBEREG = "0") as test1 left join (select UNITID, F1B01 from f1213_f1a) as test2 on test1.UNITID = test2.UNITID WHERE NOT F1B01 IS NULL and not F1B01 = 0 group by test1.instnm order by F1B01 asc limit 10';
+	$a5 = 'select instnm, F1B01 from (select UNITID, instnm from hd2013 where OBEREG = "0") as test1 left join (select UNITID, F1B01 from f1213_f1a) as test2 on test1.UNITID = test2.UNITID WHERE NOT F1B01 IS NULL and not F1B01 = 0 group by test1.instnm order by F1B01 desc limit 10';
 
+
+/*
+	$array = array ('Endowment'=>$a1,'Total Current Assets'=>$a2,'Total current liabilities'=>$a3,'Lowest non-zero tuition'=>$a4,'Highest Tuition'=>$a5);
+	
+	foreach($array as $title => $sql)
+	{
+		echo $title;
+		
+		$num=1;
+		echo "<table class='imagetable'  border='1' style='width:100%' table-layout: fixed>";
+	
+		foreach($db->query($sql) as $row)
+		{
+			$remove=0;
+        		
+			foreach($row as $x)
+			{
+        			unset($row[$remove]);
+                		$remove++;
+        		}
+			
+			echo "<tr>";
+        		echo "<td>";
+        		echo ($title);
+        		echo "</td>";
+        		echo "<td>";
+        		echo ("Result Number ".$num);
+        		echo "</td>";
+        		echo "</tr>";
+				
+			foreach ($row as $key => $value)
+        		{
+        			echo "<tr>";
+                		echo "<td>";
+                		echo ($key);
+                		echo "</td>";
+                		echo "<td>";
+                		echo ($value);
+                		echo "</td>";
+                		echo "</tr>";
+        		}
+
+			$num++;
+        		echo("<tr><td><br></td><td><br></td></tr>");		
+
+
+		}
+		
+		echo "</table>";
+		echo '<br><br>';
+	}	
+}
+
+
+*/
 ?>
